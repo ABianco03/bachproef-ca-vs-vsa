d96c8edf9f267d47e37336e118af46e12bef3df1
ABianco03 <andrej.bianco@student.hogent.be>
2026-08-17 16:03:32 +0200

    VSA: add first endpoint createProduct

---
+++ vsa-fastendpoints/VSA.FastEndpoints.OrderMgmt/VSA.FastEndpoints.OrderMgmt/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using VSA.FastEndpoints.OrderMgmt.Features.Products.Create;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products => Set<Product>();
+    //public DbSet<Customer> Customers => Set<Customer>();
+    //public DbSet<Order> Orders => Set<Order>();
+}
---
+++ vsa-fastendpoints/VSA.FastEndpoints.OrderMgmt/VSA.FastEndpoints.OrderMgmt/Features/Products/Create/CreateProductEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+
+namespace VSA.FastEndpoints.OrderMgmt.Features.Products.Create;
+public record CreateProductRequest(string Name, decimal Price, int StockQuantity);
+public record CreateProductResponse(int Id, string Name, decimal Price, int StockQuantity);
+
+public class CreateProductValidator : Validator<CreateProductRequest>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Price).GreaterThan(0);
+        RuleFor(x => x.StockQuantity).GreaterThanOrEqualTo(0);
+    }
+}
+
+public class CreateProductEndpoint : Endpoint<CreateProductRequest, CreateProductResponse>
+{
+    private readonly AppDbContext _context;
+
+    public CreateProductEndpoint(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public override void Configure()
+    {
+        Post("/api/products");
+        AllowAnonymous(); // later vervangen door je JWT-vereiste uit de NFR's
+    }
+
+    public override async Task HandleAsync(CreateProductRequest req, CancellationToken ct)
+    {
+        var product = new Product
+        {
+            Name = req.Name,
+            Price = req.Price,
+            StockQuantity = req.StockQuantity
+        };
+
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync(ct);
+
+        await Send.CreatedAtAsync<CreateProductEndpoint>(
+            new { id = product.Id },
+            new CreateProductResponse(product.Id, product.Name, product.Price, product.StockQuantity),
+            cancellation: ct);
+    }
+}
