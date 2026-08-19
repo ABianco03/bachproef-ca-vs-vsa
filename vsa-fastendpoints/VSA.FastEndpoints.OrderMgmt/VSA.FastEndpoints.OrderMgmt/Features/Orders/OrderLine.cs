90288b58ab2ed0c7fb2c36c35f41c1856c152807
ABianco03 <andrej.bianco@student.hogent.be>
2026-08-19 17:17:25 +0200

    removed reference to customer entity in order domain class

---
+++ vsa-fastendpoints/VSA.FastEndpoints.OrderMgmt/VSA.FastEndpoints.OrderMgmt/Features/Orders/OrderLine.cs
@@ -0,0 +1,9 @@
+﻿namespace VSA.FastEndpoints.OrderMgmt.Features.Orders;
+public class OrderLine
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
